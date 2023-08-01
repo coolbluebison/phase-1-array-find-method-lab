665b417bf325ae30dec5e4530d035af3b09a99ad
root <+>
2023-08-01 15:50:15 -0600

    solved

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // code your solution here
+
+
+
+
+function superbowlWin(anArray) {
+    //each object has 2 properties
+    // year and result
+    // it should use find to teach each object to see if the result is "W"
+    // it should return the year
+    // if no win is found, return undefined
+
+
+    function winner(innerArray) {
+        return (innerArray["result"]==="W")
+    }
+
+
+    let win = anArray.find(winner)
+
+
+    return win ? win["year"] : undefined;
+
+}
+
+
+
+    
+
+
+
